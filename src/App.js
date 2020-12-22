031d89de855c88e2943effe6b0a1b2e1a02b3fbc
Svetlana Ryabova <ganyim@inbox.ru>
2020-12-22 16:08:52 +0500

    add moment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 
 import React from 'react';
+import moment from 'moment';
+import 'moment/locale/ru'
 
 function DateTime(props) {
     return (
@@ -8,47 +10,43 @@ function DateTime(props) {
     )
 }
 
-const LogDateTime = function DateTimePretty(Component) {
+function dateTimePretty(Component) {
     return class extends React.Component {
         render() {
             let { date } = this.props;
-            const newdate = new Date(date);
-            let time = newdate.getHours() * 60 + newdate.getMinutes()
-            if (time < 60) time = '12 минут назад';
-            if (time > 60 && time < 1140) time = '5 часов назад';
-            if (time >= 1140) time = 'X дней назад';
-            date = time;
-            const props = {
-                date: date,
+           
+            const propsdate = {
+                date: moment(date).startOf('hour').fromNow(),
             }
-            console.log(this.props.setState)
-            return <Component {...this.props} {...props} />
+            
+            return <Component {...this.props} {...propsdate} />
         }
     }
-}(DateTime)
+}
 
-// const LogDateTime = DateTimePretty(DateTime)
+const WithDateTime = dateTimePretty(DateTime)
 
 function Video(props) {
     return (
         <div className="video">
             <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen title={props.date}></iframe>
-            <LogDateTime date={props.date} />
+            <WithDateTime date={props.date} />
         </div>
     )
 }
 
 function VideoList(props) {
-    return props.list.map(item => <Video url={item.url} date={item.date} key={props.date} />);
+    return props.list.map(item => <div key = {item.id}><Video url={item.url} date={item.date} /> </div>);
 }
 
 export default function App() {
+    moment.locale('ru');
     const list = [
-        {
+        {   id: 1,
             url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2019-07-31 13:24:00'
+            date: '2020-12-22 13:24:00'
         },
-        {
+        {   id: 2,
             url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
             date: '2020-12-08 00:10:00'
         },
@@ -56,17 +54,17 @@ export default function App() {
             url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
             date: '2020-02-03 23:16:00'
         },
-        {
+        {   id: 3,
             url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
             date: '2020-01-03 12:10:00'
         },
-        {
+        {   id: 4,
             url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2020-01-01 16:17:00'
+            date: '2020-12-20 00:10:00'
         },
-        {
+        {   id: 5,
             url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2019-12-02 05:24:00'
+            date: '2020-12-22 05:24:00'
         },
     ];
 
